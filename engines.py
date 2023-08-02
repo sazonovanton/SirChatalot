77b72c44669b60bcb33ab8e6e7720e8bf6cf0e4a
Anton <sazan95@gmail.com>
2023-08-02 14:44:55 +0000

    [Feature] Text Gen WebUI placeholder added

--- engines.py
+++ engines.py
@@ -284,6 +284,7 @@ class OpenAIEngine:
             return None
         
 
+
 ######## YandexGPT Engine ########
 
 class YandexEngine:
@@ -293,6 +294,7 @@ class YandexEngine:
         Available: text generation
         '''
         import requests 
+        self.requests = requests
 
         self.text_initiation, self.speech_initiation = text, speech
         self.text_init() if self.text_initiation else None
@@ -302,8 +304,6 @@ class YandexEngine:
         '''
         Initialize Yandex API for text generation
         '''
-        import requests
-        self.requests = requests
         import configparser
         self.config = configparser.SafeConfigParser({
             "ChatEndpoint": "https://llm.api.cloud.yandex.net/llm/v1alpha/chat",
@@ -313,6 +313,7 @@ class YandexEngine:
             "Temperature": 700,
             "MaxTokens": 1500,
             "instructionText": "You are a helpful chatbot assistant named Sir Chatalot.",
+            "LogChats": False,
             })
         self.config.read('./data/.config') 
         self.chat_vars = {} 
@@ -328,7 +329,7 @@ class YandexEngine:
         self.chat_vars['instructionText'] = self.config.get("YandexGPT", "instructionText")
 
         self.system_message = self.chat_vars['instructionText']
-        self.log_chats = False
+        self.log_chats = self.chat_vars['LogChats']
         self.max_tokens = self.chat_vars['MaxTokens']
         self.model_prompt_price = 0
         self.model_completion_price = 0
@@ -388,9 +389,13 @@ class YandexEngine:
             logger.debug('Message from user ' + str(id) + ' was sent to Yandex API')
             # check if response is successful
             if response.status_code != 200:
-                logger.error(f'Could not send message to Yandex API, response status code: {response.status_code}, response: {response.json()}')
-                user_message = 'Sorry, something went wrong. Please try again later.'
-                return user_message, messages, None
+                if response.status_code == 429 and attempt == 0:
+                    # TODO: figure out correct error if messages length is too long
+                    attempt += 1
+                else:
+                    logger.error(f'Could not send message to Yandex API, response status code: {response.status_code}, response: {response.json()}')
+                    user_message = 'Sorry, something went wrong. Please try to /delete and /start again.'
+                    return user_message, messages, None
             if attempt == 1:
                 logger.warning(f'Session is too long for user {id}, summarrizing and sending last message')
                 # summary messages
@@ -507,3 +512,287 @@ class YandexEngine:
         except Exception as e:
             logger.exception('Could not count tokens in text')
             return None
+        
+
+######## Text Generation WebUI Engine ########
+
+class TextGenEngine:
+    def __init__(self, text=False, speech=False):
+        '''
+        Initialize Text Generation WebUI API Engine
+        '''
+        self.text = text
+        self.speech = speech
+        import requests
+        import json
+        self.requests = requests
+        self.json = json
+
+        self.text_initiation = text
+        self.text_init() if self.text_initiation else None
+        
+        if self.speech:
+            logger.warning('Speech to Text is not supported in Text Generation WebUI Engine')
+
+    def text_init(self):
+        '''
+        Initialize Text Generation WebUI API Engine
+        '''
+        import configparser
+        self.config = configparser.SafeConfigParser({
+            "Host": "127.0.0.1:5000",
+            "SSL": False,
+            "max_new_tokens": 250,
+            "mode": "chat",
+            "character": "Sir Chatalot",
+            "instruction_template": '',
+            "user_name": 'User',
+            "prompt": "Your name is Sir Chatalot. You are helpful AI assistaint.",
+            "LogChats": False,
+            "chat-instruct_command": 'Continue the chat dialogue below. Write a single reply for the character "<|character|>".\n<|prompt|>',
+            'preset': '',
+            'do_sample': True,
+            'temperature': 0.7,
+            'top_p': 0.1,
+            'typical_p': 1,
+            'epsilon_cutoff': 0,  
+            'eta_cutoff': 0,  
+            'tfs': 1,
+            'top_a': 0,
+            'repetition_penalty': 1.18,
+            'repetition_penalty_range': 0,
+            'top_k': 40,
+            'min_length': 0,
+            'no_repeat_ngram_size': 0,
+            'num_beams': 1,
+            'penalty_alpha': 0,
+            'length_penalty': 1,
+            'early_stopping': False,
+            'mirostat_mode': 0,
+            'mirostat_tau': 5,
+            'mirostat_eta': 0.1,
+
+            'seed': -1,
+            'add_bos_token': True,
+            'truncation_length': 2048,
+            'ban_eos_token': False,
+            'skip_special_tokens': True,
+            'stopping_strings': []
+            })
+        self.config.read('./data/.config') 
+        self.chat_vars = {} 
+        self.chat_vars['Host'] = self.config.get('TextGenWebUI', 'Host')
+        self.chat_vars['SSL'] = self.config.getboolean('TextGenWebUI', 'SSL')
+        self.chat_vars['URI'] = 'https://' if self.chat_vars['SSL'] else 'http://' + self.chat_vars['Host'] + '/api/v1/chat'
+        self.chat_vars['max_new_tokens'] = self.config.getint('TextGenWebUI', 'max_new_tokens')
+        self.chat_vars['mode'] = self.config.get('TextGenWebUI', 'mode')
+        self.chat_vars['character'] = self.config.get('TextGenWebUI', 'character')
+        self.chat_vars['user_name'] = self.config.get('TextGenWebUI', 'user_name')
+        self.chat_vars['instruction_template'] = self.config.get('TextGenWebUI', 'instruction_template')
+        self.chat_vars['instruction_template'] = None if self.chat_vars['instruction_template'] == '' else self.chat_vars['instruction_template']
+        self.chat_vars['chat-instruct_command'] = self.config.get('TextGenWebUI', 'chat-instruct_command')
+        self.chat_vars['prompt'] = self.config.get('TextGenWebUI', 'prompt')
+        self.chat_vars['preset'] = self.config.get('TextGenWebUI', 'preset')
+        self.chat_vars['preset'] = None if self.chat_vars['preset'] == '' else self.chat_vars['preset']
+        self.chat_vars['do_sample'] = self.config.getboolean('TextGenWebUI', 'do_sample')
+        self.chat_vars['temperature'] = self.config.getfloat('TextGenWebUI', 'temperature')
+        self.chat_vars['top_p'] = self.config.getfloat('TextGenWebUI', 'top_p')
+        self.chat_vars['typical_p'] = self.config.getfloat('TextGenWebUI', 'typical_p')
+        self.chat_vars['epsilon_cutoff'] = self.config.getfloat('TextGenWebUI', 'epsilon_cutoff')
+        self.chat_vars['eta_cutoff'] = self.config.getfloat('TextGenWebUI', 'eta_cutoff')
+        self.chat_vars['tfs'] = self.config.getfloat('TextGenWebUI', 'tfs')
+        self.chat_vars['top_a'] = self.config.getfloat('TextGenWebUI', 'top_a')
+        self.chat_vars['repetition_penalty'] = self.config.getfloat('TextGenWebUI', 'repetition_penalty')
+        self.chat_vars['repetition_penalty_range'] = self.config.getfloat('TextGenWebUI', 'repetition_penalty_range')
+        self.chat_vars['top_k'] = self.config.getint('TextGenWebUI', 'top_k')
+        self.chat_vars['min_length'] = self.config.getint('TextGenWebUI', 'min_length')
+        self.chat_vars['no_repeat_ngram_size'] = self.config.getint('TextGenWebUI', 'no_repeat_ngram_size')
+        self.chat_vars['num_beams'] = self.config.getint('TextGenWebUI', 'num_beams')
+        self.chat_vars['penalty_alpha'] = self.config.getfloat('TextGenWebUI', 'penalty_alpha')
+        self.chat_vars['length_penalty'] = self.config.getfloat('TextGenWebUI', 'length_penalty')
+        self.chat_vars['early_stopping'] = self.config.getboolean('TextGenWebUI', 'early_stopping')
+        self.chat_vars['mirostat_mode'] = self.config.getint('TextGenWebUI', 'mirostat_mode')
+        self.chat_vars['mirostat_tau'] = self.config.getfloat('TextGenWebUI', 'mirostat_tau')
+        self.chat_vars['mirostat_eta'] = self.config.getfloat('TextGenWebUI', 'mirostat_eta')
+        self.chat_vars['seed'] = self.config.getint('TextGenWebUI', 'seed')
+        self.chat_vars['add_bos_token'] = self.config.getboolean('TextGenWebUI', 'add_bos_token')
+        self.chat_vars['truncation_length'] = self.config.getint('TextGenWebUI', 'truncation_length')
+        self.chat_vars['ban_eos_token'] = self.config.getboolean('TextGenWebUI', 'ban_eos_token')
+        self.chat_vars['skip_special_tokens'] = self.config.getboolean('TextGenWebUI', 'skip_special_tokens')
+        self.chat_vars['stopping_strings'] = self.config.get('TextGenWebUI', 'stopping_strings').split(',')
+        self.chat_vars['LogChats'] = self.config.getboolean('TextGenWebUI', 'LogChats')
+        
+        self.system_message = self.chat_vars['chat-instruct_command']
+        self.log_chats = self.chat_vars['LogChats']
+        self.max_tokens = self.chat_vars['max_new_tokens']
+        self.model_prompt_price = 0
+        self.model_completion_price = 0
+        
+    def count_tokens(self, messages):
+        '''
+        Count tokens in messages via tiktoken
+        '''
+        try:
+            # Get the encoding for the model
+            encoding = tiktoken.encoding_for_model(self.model)
+            # Count the number of tokens
+            tokens = 0
+            for message in messages:
+                text = message['role'] + ': ' + message['content']
+                tokens += len(encoding.encode(text))
+            return tokens
+        except Exception as e:
+            logger.exception('Could not count tokens in text')
+            return None
+        
+    def format_messages(self, messages):
+        '''
+        Format messages for Text Generation WebUI API
+        From: [
+            {"role": "user", "content": "Hi there"}
+            {"role": "assistaint", "content": "Hello, I am an AI. How can i help you?"}
+            ...]
+        To: {
+                internal: [ ["Hi there","Hello, I am an AI. How can i help you?"], ["Please explain how language models work",""] ],
+                visible: [ ["Hi there","Hello, I am an AI. How can i help you?"], ["Please explain how language models work",""] ]
+            }
+        Also delete message with role "system"
+        '''
+        try:
+            internal = []
+            visible = []
+            # message group:
+            message_group = {'user': '', 'assistant': ''}
+            user_input = messages[-1]['content'] if messages[-1]['role'] == 'user' else ''
+            messages = messages[:-1]
+            for message in messages:
+                if message['role'] == 'system':
+                    continue
+                elif message['role'] == 'user':
+                    message_group['user'] = message['content']
+                elif message['role'] == 'assistant':
+                    message_group['assistant'] = message['content']
+                    internal.append([message_group['user'], message_group['assistant']])
+                    visible.append([message_group['user'], message_group['assistant']])
+                    message_group = {'user': '', 'assistant': ''}
+            if message_group['user'] != '' or message_group['assistant'] != '':
+                internal.append([message_group['user'], message_group['assistant']])
+                visible.append([message_group['user'], message_group['assistant']])
+            history = {'internal': internal, 'visible': visible}
+            return user_input, history
+        except Exception as e:
+            logger.exception('Could not format messages for Text Generation WebUI API')
+            raise Exception('Could not format messages for Text Generation WebUI API')
+
+    def chat(self, messages, id=0, attempt=0):
+        '''
+        Chat with Text Generation WebUI API
+        Input id of user and message
+        Input:
+          * id - id of user
+          * messages = [
+                {"role": "user", "content": "Hello, how are you?"},
+                {"role": "assistant", "content": "I am fine, how are you?"},
+                ...]
+          * attempt - attempt to send message
+        Output:
+            * response - response from GPT (just text of last reply)
+            * messages - messages from GPT (all messages - list of dictionaries with last message at the end)
+            * tokens - number of tokens used in response (dict - {"prompt": int, "completion": int})
+            If not successful returns None
+        '''
+        try:
+            completion_tokens = 0
+            prompt_tokens = 0
+            tokens = self.chat_vars['max_new_tokens']
+            # # count tokens in messages
+            # tokens = self.count_tokens(messages)
+            # if tokens is not None:
+            #     tokens = self.chat_vars['max_new_tokens'] - tokens
+            #     tokens = max(tokens, 30)
+            # else:
+            #     tokens = self.chat_vars['max_new_tokens'] // 2
+            # format messages
+            user_input, history = self.format_messages(messages)
+            user_name = None
+            # format request
+            request = {
+                'user_input': user_input,
+                'max_new_tokens': tokens,
+                'history': history,
+                'mode': self.chat_vars['mode'],
+                'character': self.chat_vars['character'],
+                'instruction_template': self.chat_vars['instruction_template'],
+                # 'context_instruct': '',  # Optional
+                'your_name': user_name,
+                # 'regenerate': False,
+                # '_continue': False,
+                # 'stop_at_newline': False,
+                # 'chat_generation_attempts': 1,
+                'chat-instruct_command': self.chat_vars['chat-instruct_command'],
+                'do_sample': self.chat_vars['do_sample'],
+                'temperature': self.chat_vars['temperature'],
+                'top_p': self.chat_vars['top_p'],
+                'typical_p': self.chat_vars['typical_p'],
+                'epsilon_cutoff': self.chat_vars['epsilon_cutoff'],
+                'eta_cutoff': self.chat_vars['eta_cutoff'],
+                'tfs': self.chat_vars['tfs'],
+                'top_a': self.chat_vars['top_a'],
+                'repetition_penalty': self.chat_vars['repetition_penalty'],
+                'repetition_penalty_range': self.chat_vars['repetition_penalty_range'],
+                'top_k': self.chat_vars['top_k'],
+                'min_length': self.chat_vars['min_length'],
+                'no_repeat_ngram_size': self.chat_vars['no_repeat_ngram_size'],
+                'num_beams': self.chat_vars['num_beams'],
+                'penalty_alpha': self.chat_vars['penalty_alpha'],
+                'length_penalty': self.chat_vars['length_penalty'],
+                'early_stopping': self.chat_vars['early_stopping'],
+                'mirostat_mode': self.chat_vars['mirostat_mode'],
+                'mirostat_tau': self.chat_vars['mirostat_tau'],
+                'mirostat_eta': self.chat_vars['mirostat_eta'],
+                'seed': self.chat_vars['seed'],
+                'add_bos_token': self.chat_vars['add_bos_token'],
+                'truncation_length': self.chat_vars['truncation_length'],
+                'ban_eos_token': self.chat_vars['ban_eos_token'],
+                'skip_special_tokens': self.chat_vars['skip_special_tokens'],
+                'stopping_strings': self.chat_vars['stopping_strings'],
+            }
+            # send request
+            response = self.requests.post(self.chat_vars['URI'], json=request)
+            if response.status_code == 200:
+                result = response.json()['results'][0]['history']
+                # print(self.json.dumps(result, indent=4))
+                response = result['visible'][-1][1]
+                messages = messages.append({'role': 'assistant', 'content': response})
+            else:
+                logger.warning('Could not get response from Text Generation WebUI API')
+                response = 'Something went wrong. Please try again later.'
+            return response, messages, {'prompt': prompt_tokens, 'completion': completion_tokens}
+        except Exception as e:
+            logger.exception('Could not chat with Text Generation WebUI API')
+            return None, messages, {'prompt': 0, 'completion': 0}
+
+
+
+
+####### TEST #######
+if __name__ == '__main__':
+    # engine = OpenAIEngine(text=True)
+    # engine = YandexEngine(text=True)
+    engine = TextGenEngine(text=True)
+    messages = [
+        {"role": "system", "content": "Your name is Sir Chatalot, you are assisting the user with a task."},
+        {"role": "user", "content": "Hello, how are you?"},
+        {"role": "assistant", "content": "I am fine, how are you?"},
+        {"role": "user", "content": "I am fine too. Please tell me what is the weather like today?"},
+    ]
+    print('***        Test        ***')
+    response, messages, tokens = engine.chat(messages=messages, id=0)
+    print('============================')
+    print(response)
+    print('------------------')
+    for message in messages:
+        print(message['role'], ':', message['content'])
+    print('----------------------------')
+    print(tokens)
+    print('============================')
