902d416e44ef54d831a51b2d9306a6a57e768dc7
Anton <sazan95@gmail.com>
2024-03-21 19:40:06 +0200

    [Fix] Double messages in history sequence

--- chatutils/processing.py
+++ chatutils/processing.py
@@ -442,11 +442,8 @@ class ChatProc:
                                 response = 'Sorry, something went wrong.'
                                 logger.error(f'Function was called, but image was not generated: {response}')
             else:
-                # add to chat history
-                await self.add_to_chat_history(
-                    id=id, 
-                    message={"role": "assistant", "content": response}
-                    )
+                # save chat history
+                await self.save_chat(id=id, messages=messages)
             return response
         except Exception as e:
             logger.exception('Could not get answer to message: ' + message + ' from user: ' + str(id))
